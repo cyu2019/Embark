45fd9b186490a58f975e5835a0337b6f4faf150d
cyu2019 <cyu2019@pingry.org>
2019-05-07 07:46:53 -0400

    rope fixes

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -17,7 +17,6 @@ const GRAVITY = 25
 const JUMP = -500
 
 var rope_point = Vector2(0,0)
-var rope_dist = 0
 
 var maxAirJumps = 1
 var airJumps = maxAirJumps
@@ -27,20 +26,7 @@ func _physics_process(delta):
 	motion.y += GRAVITY
 	var onFloor = $FloorCast.is_colliding()
 	
-	if Input.is_action_pressed("ui_right"):
-		motion.x = max(GROUNDSPEED,motion.x)
-		$Sprite.flip_h = false
-		if onFloor:
-			$Sprite.play("Run")
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = -max(abs(motion.x),GROUNDSPEED)
-		$Sprite.flip_h = true
-		if onFloor:
-			$Sprite.play("Run")
-	else:
-		if onFloor:
-			$Sprite.play("Idle")
-		motion.x = 0
+	
 		
 	"""
 	if motion.y < 0:
@@ -63,24 +49,38 @@ func _physics_process(delta):
 		var result = space_state.intersect_ray(self.position, get_viewport().get_mouse_position(),[self])
 		if result:
 			rope_point = result.position
-			rope_dist = self.position.distance_to(rope_point)
 			$Line2D.visible = true
 		else:
 			$Line2D.visible = false
+		motion = Vector2(0,0)
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		
 		$Line2D.set_point_position(1,rope_point-self.position)
-		if self.position.distance_to(rope_point) >= rope_dist:
+		var dir = (rope_point - self.position)/self.position.distance_to(rope_point)
+		var angle = Vector2(0,1).angle_to(-dir)
+		var mag = GRAVITY*sin(angle)
+		var sig = sign(motion.x)
+		print(dir.rotated(3.1415926/2))
+		motion = dir.rotated(3.1415926/2)*(sig*sqrt(motion.x*motion.x+motion.y*motion.y)+mag)
+		print(motion)
+		print(angle)
 			
-			motion.y = GRAVITY
-			var dir = (rope_point - self.position)/self.position.distance_to(rope_point)
-			var mag = -cos((-dir).angle_to(Vector2(0,-GRAVITY)))*GRAVITY
-			#print(mag)
-			#print (dir*mag)
-			motion += dir*mag
-			print(motion)
 	else:
 		$Line2D.visible = false
+		if Input.is_action_pressed("ui_right"):
+			motion.x = max(GROUNDSPEED,motion.x)
+			$Sprite.flip_h = false
+			if onFloor:
+				$Sprite.play("Run")
+		elif Input.is_action_pressed("ui_left"):
+			motion.x = -max(abs(motion.x),GROUNDSPEED)
+			$Sprite.flip_h = true
+			if onFloor:
+				$Sprite.play("Run")
+		else:
+			if onFloor:
+				$Sprite.play("Idle")
+			motion.x = 0
 	
 	motion = move_and_slide(motion,UP)
 	
